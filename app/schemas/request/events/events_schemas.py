d930298edeec48ea994203501cae11cbadc3309f
AleksandrMeshkov <mrkethss@gmail.com>
2025-04-27 01:49:01 +0700

    finish_pls

--- app/schemas/request/events/events_schemas.py
+++ app/schemas/request/events/events_schemas.py
@@ -1,60 +1,40 @@
-from pydantic import BaseModel, validator
-from datetime import date, time
-from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+from datetime import datetime, time, date
 
-class EventBase(BaseModel):
+class EventCreate(BaseModel):
+    UserID: int
+    PlatformID: int
     Name: str
     City: str
-    DateStart: date  # Используем тип date вместо datetime
-    DateEnd: date    # Используем тип date вместо datetime
-    TimeStart: str   # Используем строку для времени
-    TimeEnd: str     # Используем строку для времени
+    DateStart: date
+    DateEnd: date
+    TimeStart: time
+    TimeEnd: time
     Description: str
     Address: str
 
-    @validator('TimeStart', 'TimeEnd')
-    def validate_time_format(cls, v):
-        try:
-            hours, minutes, seconds = map(int, v.split(':'))
-            if not (0 <= hours < 24 and 0 <= minutes < 60 and 0 <= seconds < 60):
-                raise ValueError("Неверное время")
-            return v
-        except ValueError:
-            raise ValueError("Время должно быть в формате HH:MM:SS")
-        except AttributeError:
-            raise ValueError("Время должно быть строкой в формате HH:MM:SS")
-
-class EventCreate(EventBase):
-    UserID: int
-    PlatformID: int
-
 class EventUpdate(BaseModel):
-    UserID: Optional[int] = None
-    PlatformID: Optional[int] = None
-    Name: Optional[str] = None
-    City: Optional[str] = None
-    DateStart: Optional[date] = None
-    DateEnd: Optional[date] = None
-    TimeStart: Optional[str] = None
-    TimeEnd: Optional[str] = None
-    Description: Optional[str] = None
-    Address: Optional[str] = None
+    UserID: int = None
+    PlatformID: int = None
+    Name: str = None
+    City: str = None
+    DateStart: date = None
+    DateEnd: date = None
+    TimeStart: time = None
+    TimeEnd: time = None
+    Description: str = None
+    Address: str = None
 
-    @validator('TimeStart', 'TimeEnd')
-    def validate_time_format(cls, v):
-        if v is None:
-            return v
-        try:
-            hours, minutes, seconds = map(int, v.split(':'))
-            if not (0 <= hours < 24 and 0 <= minutes < 60 and 0 <= seconds < 60):
-                raise ValueError("Неверное время")
-            return v
-        except ValueError:
-            raise ValueError("Время должно быть в формате HH:MM:SS")
-        except AttributeError:
-            raise ValueError("Время должно быть строкой в формате HH:MM:SS")
-
-class EventResponse(EventBase):
+class EventResponse(BaseModel):
     EventID: int
     UserID: int
     PlatformID: int
+    Name: str
+    City: str
+    DateStart: date
+    DateEnd: date
+    TimeStart: time  # Используем тип time
+    TimeEnd: time    # Используем тип time
+    Description: str
+    Address: str
